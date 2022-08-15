f2c7d56182277303dea02a2b048a6497cded24fe
Anton V <antonnugabest@gmail.com>
2022-08-15 22:09:45 +0300

    Add first section of the second page and prepare component for the third page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,16 +2,15 @@ import Mainscreen from '../mainScreen/Mainscreen';
 import AboutUs from '../aboutUs/AboutUs';
 import OurBest from '../ourBest/OurBest';
 import Footer from '../footer/Footer';
-import SecondaryScreen from '../secondaryScreen/SecondaryScreen';
 
 function App() {
   return (
     <>
-      {/* <Mainscreen/>
+      {/* <Mainscreen background = 'primary'/>
       <AboutUs/>
       <OurBest/>
-      <Footer/> */}
-      <SecondaryScreen title='Our Coffee'/>
+      <Footer/>  */}
+      <Mainscreen background = 'secondary' />
     </>
   );
 }
--- src/components/mainScreen/Mainscreen.js
+++ src/components/mainScreen/Mainscreen.js
@@ -4,17 +4,32 @@ import Btn from "../btn/Btn";
 
 import styles from './mainscreen.module.scss';
 
-const Mainscreen = () => {
+const Mainscreen = ({background}) => {
+    const additionalInfo = 
+    <>
+        <CoffeeBeans colorWhite/>
+        <h2 className={styles.mainscreen__subtitle}>We makes every day full of energy and taste</h2>
+        <h2 className={`${styles.mainscreen__subtitle} ${styles.mainscreen__subtitle_mt20}`}>Want to try our beans?</h2>
+        <Btn/>
+    </>
+    
+    let backgroundStyles; 
+    if(background === 'primary'){
+        backgroundStyles = styles.mainscreen;
+    } else if(background === 'secondary') {
+        backgroundStyles = styles.secondary;
+    }else if(background === 'tertiary') {
+        backgroundStyles = styles.tertiary;
+    } else {
+        return;
+    }
     return (
-       <main className={styles.mainscreen}>
+       <main className={backgroundStyles}>
             <header >
                 <Navigation/>
             </header>
             <h1 className={styles.mainscreen__title}>Everything You Love About Coffee</h1>
-            <CoffeeBeans colorWhite/>
-            <h2 className={styles.mainscreen__subtitle}>We makes every day full of energy and taste</h2>
-            <h2 className={`${styles.mainscreen__subtitle} ${styles.mainscreen__subtitle_mt20}`}>Want to try our beans?</h2>
-            <Btn/>
+            {background === 'primary' && additionalInfo}
        </main>
     )
 };
